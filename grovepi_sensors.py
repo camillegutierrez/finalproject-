7f0388b96530fa7c348ea97a86032ff33576cc0d
samtaylor9 <samantrt@usc.edu>
2021-05-02 14:38:37 -0700

    comment grovepi file

--- grovepi_sensors.py
+++ grovepi_sensors.py
@@ -1,28 +1,20 @@
-""" EE 250L Lab 02: GrovePi Sensors """
+""" EE 250L Final Project: Rpi-Client Side """
 
-"""python3 interpreters in Ubuntu (and other linux distros) will look in a
-default set of directories for modules when a program tries to `import` one.
-Examples of some default directories are (but not limited to):
-  /usr/lib/python3.5
-  /usr/local/lib/python3.5/dist-packages
-The `sys` module, however, is a builtin that is written in and compiled in C for
-performance. Because of this, you will not find this in the default directories.
-"""
+# import libraries
 import sys
 import time
+import grovepi
+from grove_rgb_lcd import *
+from grovepi import *
+import socket,json
+import numpy as np
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `import grovepi`
 sys.path.append('../../Software/Python/')
 # This append is to support importing the LCD library.
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
-import grovepi
-from grove_rgb_lcd import *
-from grovepi import *
 
-"""This if-statement checks if you are running this python file directly. That
-is, if you run `python3 grovepi_sensors.py` in terminSal, this if-statement will
-be true"""
 if __name__ == '__main__':
 
     light_sensor = 0 # Connect the Grove Light Sensor to analog port A0
@@ -32,11 +24,10 @@ if __name__ == '__main__':
     grovepi.pinMode(sound_sensor, "INPUT")
     time.sleep(1)
 
-    import socket,json
-    import numpy as np
+    # Server IP address and port #
     UDP_IP = "52.152.229.29"
     UDP_PORT = 8080
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Connection
 
 
     while True:
@@ -45,23 +36,24 @@ if __name__ == '__main__':
             now = time.time()
             sound_values = []
             while (time.time() - now) < 0.5:
-                # Read sensor value from sound_sensor
+                # Read sensor value from sound_sensor for the first 0.5 seconds of 1 second
                 sound_values += [grovepi.analogRead(sound_sensor)]
-            sound_std = np.std(sound_values)
+            sound_std = np.std(sound_values) # Take standard deviation of sound values
             # Read sensor value from light_sensor
             light_value = grovepi.analogRead(light_sensor)
-            #print(light_value)
 
+            # Store data in json format and send to cloud
             data = {"sound":sound_std, "light": light_value}
             datastring = json.dumps(data)
             print(datastring)
-            # ADD: Send light and sound data to the cloud
             s.sendto(datastring.encode(), (UDP_IP, UDP_PORT))
 
+            # Receive intruder information from the cloud
             intruder, addr = s.recvfrom(1024)
             intruder = intruder.decode()
             print("Intruder?: %s" % intruder)
 
+            # Update LCD screen based on intruder information
             if intruder == "Intruder":
                 setText_norefresh("Intruder, beware")
                 setRGB(255,0,0)
